a40e29e573db8597772a58df5c0266326d61ccef
Keane <keaneathallah960@gmail.com>
2024-11-12 15:29:35 +0800

    remove .env

--- database/migrations/2024_11_08_154355_create_asphalt_street_data_table.php
+++ database/migrations/2024_11_08_154355_create_asphalt_street_data_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('asphalt_street_data', function (Blueprint $table) {
             $table->id();
             $table->foreignId('asphalt_street_id');
+            $table->string('image')->nullable();
             $table->text('koordinat')->nullable();
             $table->string('permukaanPerkerasan');
             $table->string('kondisi');
