6f2b7e86c8c75fb2bc54bf0193bbbb188b56f296
Keane <keaneathallah960@gmail.com>
2024-11-15 20:12:19 +0800

    change popup message

--- resources/views/pages/webgis/index.blade.php
+++ resources/views/pages/webgis/index.blade.php
@@ -125,21 +125,15 @@
                             icon: (item.status === 'Pending') ? newIcon : (item.status ===
                                 'Completed') ? completedIcon : pendingIcon
                         }).bindPopup(
-                            `<div style="border: 2px solid #ddd; padding: 20px; border-radius: 10px; background-color: #f3f3f3; font-family: Arial, sans-serif;">
-    <h3 style="color: #2980b9; font-size: 22px; margin-top: 0;">${item.name}</h3>
-    <p style="font-size: 16px; color: #34495e; line-height: 1.6; margin-bottom: 10px;">
-        <strong>Alamat:</strong> ${item.address}<br>
-        <strong>Status:</strong> <span style="color: #e74c3c;">${item.status}</span><br>
-        <strong>Aspirasi:</strong> ${item.description}
-    </p>
-    <div style="margin-top: 15px; text-align: center;">
-        <img src="${url}/storage/${item.image}" alt="Complain Image" style="max-width: 100%; height: auto; border-radius: 8px; box-shadow: 0 4px 10px rgba(0,0,0,0.1);">
-    </div>
-    <div style="text-align: center; margin-top: 15px;">
-        <button style="background-color: #2980b9; color: white; border: none; padding: 10px 20px; border-radius: 5px; cursor: pointer; font-size: 16px;">See More</button>
-    </div>
-</div>
-`
+                            `<div style="font-size: 14px; line-height: 1.5;">
+                            <strong>Nama:</strong> ${item.name}<br>
+                            <strong>Alamat:</strong> ${item.address}<br>
+                            <strong>Status:</strong> ${item.status}<br>
+                            <strong>Aspirasi:</strong> ${item.description}<br>
+                            <div style="margin-top: 10px;">
+                                <img src="${url}/storage/${item.image}" alt="Complain Image" style="max-width: 100%; height: auto; border-radius: 5px;">
+                            </div>
+                        </div>`
                         );
                         layers['Complaints'].addLayer(marker);
                     }
